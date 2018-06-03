af31f5feed7aed9add0a0a8995799a5e52ac480d
StevenJBurns <stevenjburns@msn.com>
2018-06-03 17:25:21 -0600

    Stated content for Contacts page; playing with depth via box-shadow and panels

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,23 +1,57 @@
 <template>
   <div>
-    <h3>Contact</h3>
+    <h3>Drop me a line at any of the following:</h3>
+    <section>
+      <font-awesome-icon :icon="['fab', 'linkedin']" size="4x" />
+      <span>The professional me</span>
+    </section>
+    <section>
+      <font-awesome-icon :icon="['fab', 'twitter']" size="4x" />
+      <span>I try to be funny here</span>
+    </section>
+    <section>
+      <font-awesome-icon :icon="['fab', 'github']" size="4x" />
+      <span>Where I store my shit</span>
+    </section>
+    <section>
+      <font-awesome-icon icon="envelope" size="4x" />
+      <span>Compose an electronic letter to stevenjburns@msn.com</span>
+    </section>
   </div>
 </template>
 
 <script>
+  import FontAwesomeIcon from "@fortawesome/vue-fontawesome";
+  import { fasEnvelope } from "@fortawesome/fontawesome-free-solid";
+  import { fabLinkedin, fabGithub, faTwitter } from "@fortawesome/fontawesome-free-brands";
+
   export default {
     name: 'PageContact',
-    props: {}
+    props: {},
+    components: {
+      FontAwesomeIcon
+    },
   }
 </script>
 
 <style scoped>
   div {
     flex: 1 0 auto;
-    display: flex;
+    display: grid;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     background: #707090
+  }
+
+  section {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    margin: 12px auto;
+    padding: 8px;
+    border-radius: 8px;
+    width: 240px;
+    box-shadow: 4px 4px 16px #000000;
+    background: #303050
   }
 </style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <section>
     <h2>Full Stack Developer</h2>
     <svg height="192" width="192" viewBox="0 0 256 256">
       <defs>
@@ -12,15 +13,16 @@
       <circle cx="128" cy="128" r="96" fill="#F0F0F0" filter="url(#drop-shadow)">
         <animate attributeType="XML" attributeName="r" from="0" to="96" dur="0.75s" fill="freeze" />
       </circle>
-      <circle cx="128" cy="128" r="0" fill="none" stroke="#708090FF" stroke-width="24">
+      <circle cx="128" cy="128" r="0" fill="none" stroke="#405060FF" stroke-width="24">
         <animate attributeType="XML" attributeName="r" begin="0.75s" from="0" to="50" dur="0.75s" fill="freeze" />
       </circle>
-      <polygon opacity="0" points="128,128 16,84 62,62 84,16" stroke="#708090FF" stroke-width="8" stroke-linejoin="round" fill="#F0F0F0" fill-opacity="1">
+      <polygon opacity="0" points="128,128 16,84 62,62 84,16" stroke="#405060FF" stroke-width="8" stroke-linejoin="round" fill="#F0F0F0" fill-opacity="1">
         <animate attributeType="XML" attributeName="opacity" begin="1.5s" from="0" to="1" dur="0.5s" fill="freeze" />
         <animateTransform attributeName="transform" type="translate" from="-24 -24" to="0 0" begin="1.5s" dur="0.5s" fill="freeze" />
       </polygon>
     </svg>
     <h2>Longmont&nbsp;&bull;&nbsp;Colorado</h2>
+    </section>
   </div>
 </template>
 
@@ -41,6 +43,14 @@
     align-items: center;
     background: #708090;
     text-shadow: 2px 2px 16px #2F2F2F
+  }
+
+  section {
+    padding: 24px;
+    background: #405060;
+    border: 1px solid #0F0F0F;
+    box-shadow: 2px 2px 32px #000000;
+    border-radius: 8px
   }
 
   img {

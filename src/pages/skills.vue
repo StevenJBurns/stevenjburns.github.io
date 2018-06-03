d91bff16c98378e7f160919b5338d17fa88b9648
StevenJBurns <stevenjburns@msn.com>
2018-06-03 01:40:36 -0600

    Nearly working D3 Pack chart on the Skills page

--- src/pages/skills.vue
+++ src/pages/skills.vue
@@ -1,19 +1,59 @@
 <template>
   <div>
-    <h3>Skills</h3>
-    <div id="divSkillsChart"></div>
-    <!-- <skills-chart /> -->
+    <!-- <h3>Skills</h3> -->
+    <svg id="svgPackChart" height="512" width="512"></svg>
   </div>
 </template>
 
 <script>
-  //import SkilsChart from "../components/svgSkillsChart.vue";
+  import * as d3 from "d3";
+  import AppData from "../data/skillsHierarchy.json";
 
+
+  
   export default {
     name: 'PageSkills',
+    data() {
+      return {
+        colors: ["#445566", "#778899", "#AABBCC"]
+      }
+    },
     props: {},
     components: {
 
+    },
+    mounted() {
+      const svg = d3.select("#svgPackChart");
+      let g = svg.append("g").attr("transform", "translate(2,2)");
+      let format = d3.format(",d");
+      // let diameter;
+
+      let pack = d3.pack().size([512, 512]);
+      let root = d3.hierarchy(AppData).sum(d => d.size);
+
+      let node = g.selectAll(".node")
+        .data(pack(root).descendants())
+        .enter()
+        .append("g")
+        .attr("class", d => d.children ? "node" : "leaf node")
+        .attr("transform", d => `translate(${d.x}, ${d.y})`);
+
+      node.append("title").text(d => d.data.name /* + "\n" + format(d.value) */ );
+
+      node.append("circle")
+        .attr("r", d => d.r)
+        .style("fill", d => this.colors[d.depth]);
+
+      node.append("text")
+        .attr("text-anchor", "middle")
+        .attr("dy", "6")
+        .style("fill", "#DFDFDF")
+
+
+      // node.filter(d => !d.children)
+      //   .append("text")
+      //   .attr("dy", "0.3em")
+      //   .text(d => d.data.name.substring(0, d.r / 3));
     }
   }
 </script>
@@ -27,9 +67,12 @@
     background: #809070
   }
 
-  #divSkillsChart {
-    flex: 1 0 auto;
-    background: maroon
+  .node {
+    background: #445566
+  }
+
+  .leaf {
+    background: #778899
   }
 </style>
 

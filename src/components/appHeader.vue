5d02da40279360cf0e03ddd2a32fce5bfe54710a
StevenJBurns <stevenjburns@msn.com>
2018-06-03 04:56:41 -0600

    Added app logo to the header component

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,8 +1,16 @@
 <template>
   <header>
+    <svg id="appLogo" height="64" width="64" viewBox="0 0 256 256">
+      <circle cx="128" cy="128" r="96" fill="#DFDFDF">
+      </circle>
+      <circle cx="128" cy="128" r="48" fill="none" stroke="#2F2F2F" stroke-width="24">
+      </circle>
+      <polygon points="128,128 16,84 62,62 84,16" stroke="#2F2F2F" stroke-width="8" stroke-linejoin="round" fill="#F0F0F0" fill-opacity="1">
+      </polygon>
+    </svg>
     <h2>
       <router-link to="/">STEVEN J BURNS</router-link>
-      </h2>
+    </h2>
     <Nav />
     <font-awesome-icon icon="bars" size="2x" @click="openResponsiveMenu()" />
   </header>
@@ -31,11 +39,15 @@
 <style scoped>
   header {
     display: grid;
-    grid-template-columns: 1fr auto auto;
+    grid-template-columns: auto 1fr auto auto;
     padding: 12px;
     color: #DFDFDF;
     background: transparent;
     text-align: left
+  }
+
+  #appLogo {
+    display: inline-block
   }
 
   header h2 {

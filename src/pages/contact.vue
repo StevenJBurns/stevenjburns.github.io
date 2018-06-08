01f1577699c2762f8683f1b3dcb0456a1f53a933
StevenJBurns <stevenjburns@msn.com>
2018-06-08 16:57:49 -0600

    Original Particles JS module is working correctly on Contact page -- will experiment with images next and swap it with Home page background

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -27,11 +27,13 @@
         <span>Compose an electronic letter. Or just send a raven.</span>
       </section>
     </div>
+    <div id='particles-js'></div>
   </div>
 </template>
 
 <script>
   import { eventBus } from "../main.js";
+  //import particlesJS from "particles.js";
   import FontAwesomeIcon from "@fortawesome/vue-fontawesome";
   import { fasEnvelope } from "@fortawesome/fontawesome-free-solid";
   import { fabLinkedin, fabGithub, faTwitter } from "@fortawesome/fontawesome-free-brands";
@@ -46,6 +48,125 @@
     },
     created() {
       eventBus.$emit('changingTheme', this.theme)
+    },
+    mounted() {
+      let particlesJS = require('particles.js');
+      this.$nextTick(() => this.initParticlesJS());
+    },
+    methods: {
+      initParticlesJS () {
+        /* eslint-disable */
+        particlesJS('particles-js', {
+          'particles': {
+            'number': {
+              'value': 80,
+              'density': {
+                'enable': true,
+                'value_area': 800
+              }
+            },
+            'color': {
+              'value': '#ffffff'
+            },
+            'shape': {
+              'type': 'circle',
+              'stroke': {
+                'width': 0,
+                'color': '#000000'
+              },
+              'polygon': {
+                'nb_sides': 5
+              },
+              'image': {
+                'src': 'img/github.svg',
+                'width': 100,
+                'height': 100
+              }
+            },
+            'opacity': {
+              'value': 0.25,
+              'random': false,
+              'anim': {
+                'enable': false,
+                'speed': 1,
+                'opacity_min': 0.1,
+                'sync': false
+              }
+            },
+            'size': {
+              'value': 10,
+              'random': true,
+              'anim': {
+                'enable': true,
+                'speed': 48,
+                'size_min': 0.1,
+                'sync': false
+              }
+            },
+            'line_linked': {
+              'enable': true,
+              'distance': 192,
+              'color': '#FFFFFF',
+              'opacity': 0.25,
+              'width': 2
+            },
+            'move': {
+              'enable': true,
+              'speed': 2,
+              'direction': 'none',
+              'random': false,
+              'straight': false,
+              'out_mode': 'out',
+              'bounce': false,
+              'attract': {
+                'enable': false,
+                'rotateX': 600,
+                'rotateY': 1200
+              }
+            }
+          },
+          'interactivity': {
+            'detect_on': 'canvas',
+            'events': {
+              'onhover': {
+                'enable': false,
+                'mode': 'repulse'
+              },
+              'onclick': {
+                'enable': true,
+                'mode': 'push'
+              },
+              'resize': true
+            },
+            'modes': {
+              'grab': {
+                'distance': 800,
+                'line_linked': {
+                  'opacity': 1
+                }
+              },
+              'bubble': {
+                'distance': 800,
+                'size': 80,
+                'duration': 2,
+                'opacity': 0.8,
+                'speed': 3
+              },
+              'repulse': {
+                'distance': 400,
+                'duration': 0.4
+              },
+              'push': {
+                'particles_nb': 4
+              },
+              'remove': {
+                'particles_nb': 2
+              }
+            }
+          },
+          'retina_detect': true
+        })
+      }
     }
   }
 </script>
@@ -54,12 +175,15 @@
   div {
     flex: 1 0 auto;
     padding: 32px 15%;
-    background: #707090
+    background: #707090;
+    position: relative;
   }
 
   #divCardWrapper {
     display: flex;
     flex-wrap: wrap;
+    background: #00000000;
+    z-index: 1;
     justify-content: space-evenly
   }
 
@@ -100,6 +224,15 @@
   section span {
     padding: 4px;
     align-self: center
+  }
+
+  #particles-js {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 0
   }
 
   @media screen and (max-width: 720px) {

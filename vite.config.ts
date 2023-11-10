d312db1b83a050499d239c79b5efdc38b8c7d403
Steven J Burns <stevenjburns@msn.com>
2023-11-10 10:05:58 -0600

    chore: create absolute import path for assets folder; successful

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-const path = require("path");
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue2";
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue2';
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
 });
